995c8891000c2f304b1f0a438e0b62d8dd45ab23
Sam <sam@justcallmesam.com>
2024-12-30 17:12:09 -0500

    bio and formatting

--- app/page.tsx
+++ app/page.tsx
@@ -11,32 +11,30 @@ export default function Home() {
         <div className="
             flex items-center justify-center
             min-h-screen w-full
-            font-[family-name:var(--font-geist-sans)]
+            font-[family-name:var(--font-geist-mono)]
             text-neutral-800
         ">
-            <div className="flex flex-col gap-5">
-                <div className="
-                    flex flex-col gap-5 justify-center
-                    relative p-4
-                    border border-neutral-300
-                ">
-                    <div className="flex gap-2 absolute top-[108%] right-2">
-                        <IconBrandLinkedin stroke="1.5" color="#44403c" width={20} height={20}/>
-                        <IconBrandGithub stroke="1.5" color="#44403c" width={20} height={20}/>
-                        <IconBrandInstagram stroke="1.5" color="#44403c" width={20} height={20}/>
-                        <IconMail stroke="1.5" color="#44403c" width={20} height={20}/>
-                        <IconCode stroke="1.5" color="#44403c" width={20} height={20}/>
-                    </div>
+            <div className="
+                flex flex-col gap-5 justify-center
+                relative p-6
+                border border-neutral-300
+                w-[450px] max-w-[85%]
+            ">
+                <div className="flex gap-2 absolute top-[108%] right-2">
+                    <IconBrandLinkedin stroke="1.5" color="#44403c" width={20} height={20}/>
+                    <IconBrandGithub stroke="1.5" color="#44403c" width={20} height={20}/>
+                    <IconBrandInstagram stroke="1.5" color="#44403c" width={20} height={20}/>
+                    <IconMail stroke="1.5" color="#44403c" width={20} height={20}/>
+                    <IconCode stroke="1.5" color="#44403c" width={20} height={20}/>
+                </div>
 
-                    <div className="font-[family-name:var(--font-geist-mono)]">200 OK</div>
+                <div className="font-[family-name:var(--font-geist-mono)]">200 OK</div>
 
-                    <div className="flex flex-col pr-4">
+                <div className="flex flex-col gap-1">
+                    <div className="flex flex-col">
                         <div className="flex gap-2">
-                            <div className="flex gap-1 font-bold">
-                                <div className="font-bold">sam</div>
-                                <div className="font-bold">[Samuel Marks]</div>
-                            </div>
-                            <div className="italic">/sæm/</div>
+                            <div className="font-bold">sam</div>
+                            <div className="font-bold">[Samuel Marks]</div>
                         </div>
                         <a
                             className="text-blue-500"
@@ -45,8 +43,12 @@ export default function Home() {
                             sam@justcallmesam.com
                         </a>
                     </div>
-
-                    <div className="">human being, software engineer</div>
+                    <ul className="list-disc ml-10">
+                        <li>human being,</li>
+                        <li>software engineer,</li>
+                        <li>purveyor of silliness and empathy, and</li>
+                        <li>curator of colors, shapes, and sounds;</li>
+                    </ul>
                 </div>
             </div>
         </div>
