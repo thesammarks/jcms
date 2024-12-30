dbf77061ffe480a75281469fe802393d5414a1dd
Sam <sam@justcallmesam.com>
2024-12-30 17:17:28 -0500

    add responsive flexbox props

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,16 @@ import {
 export default function Home() {
     return (
         <div className="
-            flex items-center justify-center
+            flex items-center max-md:items-start justify-center
             min-h-screen w-full
             font-[family-name:var(--font-geist-mono)]
             text-neutral-800
         ">
             <div className="
                 flex flex-col gap-5 justify-center
-                relative p-6
+                relative p-8
                 border border-neutral-300
-                w-[450px] max-w-[85%]
+                w-[450px] max-w-[85%] max-md:mt-10
             ">
                 <div className="flex gap-2 absolute top-[108%] right-2">
                     <IconBrandLinkedin stroke="1.5" color="#44403c" width={20} height={20}/>
