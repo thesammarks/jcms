8438f758f7aa4f67b8bca74b422b98239d20af01
Sam <sam@justcallmesam.com>
2025-01-01 02:44:14 -0600

    add `/` to `/card` redirect and move card content to `app/card`

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import { Card } from '@mantine/core';
-import Icons from "@/app/components/Icons";
-import CardContent from "@/app/components/CardContent";
-
-export default function Home() {
+export default function App() {
     return (
-        <div className="
-            flex justify-center items-center max-md:items-start
-            min-h-screen w-full text-neutral-300
-        ">
-            <Card
-                shadow="sm"
-                padding="xl"
-                className="
-                    relative overflow-visible
-                    w-[450px] max-w-[85%] max-md:mt-10
-                    border border-neutral-300
-                ">
-                <Icons />
-                <CardContent />
-            </Card>
-        </div>
+        <div></div>
     );
 }
