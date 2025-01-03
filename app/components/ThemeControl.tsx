7e75cb0c91f74f97f154c18435cf67b6a0c79b3c
Sam <sam@justcallmesam.com>
2025-01-03 01:03:30 -0600

    move hover property to icon container

--- app/components/ThemeControl.tsx
+++ app/components/ThemeControl.tsx
@@ -13,19 +13,19 @@ export default function ThemeControl() {
             text-neutral-500 dark:text-neutral-400
         ">
             <div
-                className="relative justify-center items-center flex w-[22px] h-[22px]"
+                className="relative flex justify-center items-center w-[22px] h-[22px] hover:text-blue-500"
                 onClick={() => {setColorScheme(colorScheme === "dark" ? "light" : "dark")}}
             >
                 <Icon
                     icon={IconMoonFilled}
-                    className="absolute opacity-100 dark:opacity-0 hover:text-blue-500"
+                    className="absolute opacity-100 dark:opacity-0"
                     onPress={() => null}
                     width={18}
                     height={18}
                 />
                 <Icon
                     icon={IconSunFilled}
-                    className="absolute opacity-0 dark:opacity-100 hover:text-blue-500"
+                    className="absolute opacity-0 dark:opacity-100"
                     onPress={() => null}
                     width={20}
                     height={20}
