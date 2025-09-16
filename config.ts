54803c79b0b6f19ac48aa2bc002254a073ac5827
Sam <sam@justcallmesam.com>
2025-09-15 19:35:06 -0500

    update author email address

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ const config = {
     APPLICATION_DESCRIPTION: "A landing page for me, Sam.",
     APPLICATION_KEYWORDS: "Samuel Marks, Sam Marks, Sam, Software Engineer, SWE, Next, Next.JS, React, Mantine, Tabler icons, UI, API, Spring Boot, Java, landing page, home page, portfolio, developer, engineer",
     AUTHOR_NAME: "Samuel Marks",
-    AUTHOR_EMAIL_ADDRESS: "sam@justcallmesam.com",
+    AUTHOR_EMAIL_ADDRESS: "sam@capysoft.app",
     LINKEDIN_URL: "https://linkedin.com/in/thesammarks",
     GITHUB_URL: "https://github.com/thesammarks",
     INSTAGRAM_URL: "https://instagram.com/thesammarks",
