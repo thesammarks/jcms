90a40e4a48e02bceb74fd6404afe5b7f54c3786f
Sam <sam@justcallmesam.com>
2025-10-19 23:56:57 -0500

    tune copy button opacity

--- app/components/utils/CopyText.tsx
+++ app/components/utils/CopyText.tsx
@@ -14,11 +14,12 @@ export default function CopyText(props: CopyTextProps) {
                 <Tooltip
                     className="bg-neutral-800/30"
                     position="right"
+                    opacity={"90%"}
                     label={copied ? <Text fw={700} size="xs">Copied</Text> : <Text fw={700} size="xs">Copy</Text>}
                 >
-                    <ActionIcon variant="subtle" onClick={copy}>
-                        <IconCheck opacity={`${copied ? 100 : 0}`} className={`absolute w-[20px]`}/>
-                        <IconCopy opacity={`${copied ? 0 : 100}`} className={`absolute w-[20px]`}/>
+                    <ActionIcon variant="subtle" onClick={copy} bg={"transparent"}>
+                        <IconCheck opacity={`${copied ? "90%" : "0"}`} className={`absolute w-[20px]`}/>
+                        <IconCopy opacity={`${copied ? "0" : "90%"}`} className={`absolute w-[20px]`}/>
                     </ActionIcon>
                 </Tooltip>
             )}
